38f5457073cc8994afed07a5d1c8a0b9d8f9bc16
David Ruiz <davidl.ruizc@gmail.com>
2018-08-04 21:00:24 -0500

    ASP integracion

---
+++ QuickMenu/QuickMenu/Controllers/TemplateController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuickMenu.Controllers
+{
+    public class TemplateController : Controller
+    {
+        // GET: Template
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            using (Models.restaurantsdbEntities)
+        }
+    }
+}
